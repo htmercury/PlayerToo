c2467aad1b46f7c945f8a7112ae88f75900d25a0
Ka <kawong2020@u.northwestern.edu>
2020-02-20 16:42:17 -0600

    add migrate script for boardgames data

--- src/migrate-data-db.js
+++ src/migrate-data-db.js
@@ -1,4 +1,26 @@
 import { fireDb } from './firebase';
 import * as data from '../public/board_games.json';
 
-console.log(data);
+const games = Object.values(data);
+
+console.log(games.length);
+
+let count = 1;
+
+const uploadGames = async () => {
+    for (let boardGame of games) {
+        if (boardGame.id !== undefined) {
+            await fireDb
+                .collection('BoardGames')
+                .doc(boardGame.id)
+                .set(boardGame);
+            console.log('Entered new data into the collection ' + count);
+            
+            count++;
+        }
+    }
+};
+
+console.log('migration complete');
+
+uploadGames();
