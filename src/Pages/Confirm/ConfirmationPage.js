04f3fd9446356e2dff2256038073b5c26279f742
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-02-23 22:41:20 -0600

    loader add-in

--- src/Pages/Confirm/ConfirmationPage.js
+++ src/Pages/Confirm/ConfirmationPage.js
@@ -1,7 +1,7 @@
-import React, { useContext, createRef } from 'react';
+import React, { useContext, createRef, useEffect, useState } from 'react';
 import { AppState } from '../../context';
 import { Link, useParams } from 'react-router-dom';
-import { Header, Segment, Button, Icon, Container, Sticky } from 'semantic-ui-react';
+import { Header, Segment, Button, Icon, Container, Sticky, Loader } from 'semantic-ui-react';
 
 const ConfirmationPage = () => {
   const state = useContext(AppState);
@@ -9,7 +9,12 @@ const ConfirmationPage = () => {
   const { id } = useParams(); 
   const listing = data[id];
   const contextRef = createRef();
-  console.log(id)
+  
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
 
   return (
     <div ref={contextRef}>
@@ -17,20 +22,24 @@ const ConfirmationPage = () => {
         <PageHeader />
       </Sticky>
       <Container style={{ marginTop: "15px"}}>
-        <Header as="h1" icon>
-          <Icon color="yellow" name='check circle' />
-          We've notified {listing.lender.firstname} that you would like to borrow their game: <p style={{color: "orange" }}>{listing.game}</p>
-          <Header.Subheader>
-            If {listing.lender.firstname} accepts your booking, you will get a notification so you can arrange a meetup!
-          </Header.Subheader>
-          <br />
-          <Header.Subheader>
-            Happy playing!
-          </Header.Subheader>
-        </Header>
-        <br />
-        <br />
-        <Button content="Back to Listings" as={Link} to="/" fluid color="yellow" />
+        {loading ? <Loader active style={{marginTop: "15px" }} size="huge">Confirming boardgame request...</Loader> :
+          <React.Fragment>
+            <Header as="h1" icon>
+              <Icon color="yellow" name='check circle' />
+              We've notified {listing.lender.firstname} that you would like to borrow their game: <p style={{color: "orange" }}>{listing.game}</p>
+              <Header.Subheader>
+                If {listing.lender.firstname} accepts your booking, you will get a notification so you can arrange a meetup!
+              </Header.Subheader>
+              <br />
+              <Header.Subheader>
+                Happy playing!
+              </Header.Subheader>
+            </Header>
+            <br />
+            <br />
+            <Button content="Back to Listings" as={Link} to="/" fluid color="yellow" />
+          </React.Fragment>
+        }
       </Container>
     </div>
   );
