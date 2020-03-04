4eb5561321018216fa9b15949767f021b86aca2d
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-03-03 20:55:55 -0600

    app.js hot fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext, createRef, useRef } from 'react';
 import HomePage from './Pages/Home/HomePage';
-import HomePageLender from './Pages/Home/HomePageLender';
+import HomePageLender from './Pages/Lender/HomePageLender';
 import ListingPage from './Pages/Listing/ListingPage';
 import ConfirmationPage from './Pages/Confirm/ConfirmationPage';
 import FireStoreUsersPage from './Pages/FireStoreExamples/FireStoreUsersPage';
