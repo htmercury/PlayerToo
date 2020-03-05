8ba7c925f7d233fc493f99babb100edb32c9571a
omofish <jason_lzx@hotmail.com>
2020-03-05 10:33:09 -0600

    Prep for backend

--- src/Pages/Lender/Borrowers.js
+++ src/Pages/Lender/Borrowers.js
@@ -2,39 +2,54 @@ import React, { useState } from 'react';
 import { Card, Icon, Button, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+const BorrowerCard = ({ item }) => {
+  return (
+    <Card>
+      <Card.Content>
+        <Image circular floated="right" size="mini" src={item.image} />
+        <Card.Header>{item.borrower}</Card.Header>
+        <Card.Meta>
+          <Icon name="star" /> {item.borrowerRating}
+        </Card.Meta>
+        <Card.Description>{item.duration}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Button.Group fluid>
+          <Button
+            color="yellow"
+            labelPosition="left"
+            icon
+            as={Link}
+            to="/lender/myListings"
+          >
+            <Icon name="check" />
+            Approve
+          </Button>
+          <Button
+            color="grey"
+            labelPosition="right"
+            icon
+            as={Link}
+            to="/lender/myListings"
+          >
+            <Icon name="close" />
+            Decline
+          </Button>
+        </Button.Group>
+      </Card.Content>
+    </Card>
+  );
+};
+
 const Borrowers = ({ data }) => {
-    return (
-        <div>
-            <Card.Group centered>
-                <Card>
-                    <Card.Content>
-                        <Image
-                            floated="right"
-                            size="mini"
-                            src="https://www.theheadshotguy.co.uk/wp-content/uploads/2014/12/Screen-Shot-2014-12-02-at-11.14.42.png"
-                        />
-                        <Card.Header>James Smith</Card.Header>
-                        <Card.Meta>
-                            <Icon name="star" /> 3.6
-                        </Card.Meta>
-                        <Card.Description>13 March - 17 March</Card.Description>
-                    </Card.Content>
-                    <Card.Content extra>
-                        <Button.Group fluid>
-                            <Button color="yellow" labelPosition="left" icon>
-                                <Icon name="check" />
-                                Approve
-                            </Button>
-                            <Button color="grey" labelPosition="right" icon>
-                                <Icon name="close" />
-                                Decline
-                            </Button>
-                        </Button.Group>
-                    </Card.Content>
-                </Card>
-            </Card.Group>
-        </div>
-    );
+  return (
+    <div>
+      <Card.Group centered itemsPerRow='1'>
+        {data.map(i => 
+            <BorrowerCard item={i} />)}
+      </Card.Group>
+    </div>
+  );
 };
 
 export default Borrowers;
--- src/Pages/Lender/EachLenderListing.js
+++ src/Pages/Lender/EachLenderListing.js
@@ -1,8 +1,25 @@
 import React, { useContext, useState, createRef } from 'react';
 import { AppState } from '../../context';
 import { useParams, Link } from 'react-router-dom';
-import { Grid, Image, Button, Header, Container, Input } from 'semantic-ui-react';
+import {
+  Grid,
+  Image,
+  Button,
+  Header,
+  Container,
+  Input,
+} from 'semantic-ui-react';
 import Bookings from './Borrowers';
+
+const borrowers = [
+  {
+    borrower: 'James Smith',
+    borrowerRating: '3.6',
+    duration: '13 March - 17 March',
+    image:
+      'https://www.theheadshotguy.co.uk/wp-content/uploads/2014/12/Screen-Shot-2014-12-02-at-11.14.42.png',
+  },
+];
 
 const EachLenderListing = () => {
   const appState = useContext(AppState);
@@ -12,7 +29,7 @@ const EachLenderListing = () => {
   const { games } = appState;
   const contextRef = createRef();
   const { id } = useParams();
-  console.log("Open EachLenderListing ", id)
+  console.log('Open EachLenderListing ', id);
   const listing = games[games.findIndex(g => g.id === id)];
 
   return (
@@ -22,10 +39,10 @@ const EachLenderListing = () => {
         <Grid columns={1}>
           <Grid.Row>
             <Grid.Column>
-              <Button 
-                content="Back to Game List" 
-                icon='arrow left' 
-                color="yellow" 
+              <Button
+                content="Back to Game List"
+                icon="arrow left"
+                color="yellow"
                 fluid
                 as={Link}
                 to="/lender/myListings"
@@ -33,38 +50,34 @@ const EachLenderListing = () => {
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-            <Image.Group size="small" style={{marginLeft:"90px"}}>
+            <Image.Group size="small" style={{ marginLeft: '90px' }}>
               <Image src={Object.values(listing.images)[3]} />
             </Image.Group>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column>
-            <Header as={"h2"}>
-              {listing.name}
-            </Header>
+              <Header as={'h2'}>{listing.name}</Header>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
-          <Grid.Column>
-            <Header content="Remarks"/>
-            {/* Take note that this needs to get information from when the listing is added */}
-            <Header.Subheader content={listing.description} />
-          </Grid.Column>
+            <Grid.Column>
+              <Header content="Remarks" />
+              {/* Take note that this needs to get information from when the listing is added */}
+              <Header.Subheader content={listing.description} />
+            </Grid.Column>
           </Grid.Row>
-            
 
           <Grid.Column>
-            <Header>
-              Current Bookings
-            </Header>
-            <Bookings />
+            <Header>Current Bookings</Header>
+            <Bookings data={borrowers} />
           </Grid.Column>
-          <Grid.Row style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}>
-          </Grid.Row>
+          <Grid.Row
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          ></Grid.Row>
         </Grid>
       </Container>
     </div>
