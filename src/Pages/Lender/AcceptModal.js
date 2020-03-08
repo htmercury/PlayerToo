235fa3aae6b5edcda6ac689f68082dad995e6fef
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-03-07 20:13:50 -0600

    done! doesn't save changes but it does do mostly everything else

--- src/Pages/Lender/AcceptModal.js
+++ src/Pages/Lender/AcceptModal.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import { Modal, Button } from 'semantic-ui-react';
+import { Modal, Button, Label, Input } from 'semantic-ui-react';
 
 const AcceptModal = ({ open, setModalOpen, meetUpLocation }) => {
+    let location = meetUpLocation
+    var time =  "4:00 pm"
+    var date = "13/3/2020"
+    const saveEditsLocation = input => {
+        location = input.target.value;
+      };
+    const saveEditsTime = input => {
+        time = input.target.value;
+      };
+      const saveEditsDate = input => {
+        date = input.target.value;
+      };
   return (
     <Modal open={open}>
       <Modal.Header>
         Proposed Meet Up Details
       </Modal.Header>
       <Modal.Content>
-        {meetUpLocation}
+      <Label pointing='below'>Meeting location</Label>
+      <Input
+      defaultValue={meetUpLocation}
+      fluid
+      onChange={saveEditsLocation.bind(this)}
+    ></Input>
+    <br></br>
+     <Label pointing='below'>Meeting time</Label>
+     <Input
+      defaultValue= {time}
+      fluid
+      onChange={saveEditsTime.bind(this)}
+    ></Input>
+    <br></br>
+     <Label pointing='below'>Meeting date</Label>
+     <Input
+      defaultValue= {date}
+      fluid
+      onChange={saveEditsDate.bind(this)}
+    ></Input>
+
+
+        
       </Modal.Content>
       <Modal.Actions>
-          <Button content="Accept" negative icon="check" onClick={() => setModalOpen(false)}/>
+          <Button content="Confirm!" negative icon="check" onClick={() => setModalOpen(false)}/>
       </Modal.Actions>
     </Modal>
   );
