be9ba76bc46b1d78c8c385c9c9ad2085c30713fc
omofish <jason_lzx@hotmail.com>
2020-03-09 16:07:38 -0500

    bugfixes

--- src/Pages/Lender/EachLenderListing.js
+++ src/Pages/Lender/EachLenderListing.js
@@ -74,12 +74,19 @@ const EachLenderListing = () => {
   const listing = games[games.findIndex(g => g.id === id)];
 
   // get requests for this particular game
-  const gameRequests = myRequests.filter(x => x.game_id === id)[0].requests;
+  const gameRef = myRequests.filter(x => x.game_id === id)[0];
+  console.log("reqs", myRequests.filter(x => x.game_id === id));
+  
+  const gameRequests = ('requests' in gameRef) ? gameRef.requests : [];
 
-  // // workaround: setting isApproved to null
-  // gameRequests.forEach(gr => (gr.isApproved = null));
+  // workaround: setting isApproved to null
+  gameRequests.forEach(gr => (gr.isApproved = null));
+
+  // need to hook this up to backend
+  description = gameRef.additional_details;
 
   const [requests, setRequests] = useState(gameRequests);
+  
 
   return (
     <div ref={contextRef}>
