65e2969935e41bdb67ec3dac0d4ee47dad36b391
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2020-03-09 19:29:11 -0500

    static data replacement

--- src/client.js
+++ src/client.js
@@ -1,4 +1,6 @@
 import games from './Data/board_games';
+import listings from './Data/listings';
+import users from './Data/users';
 
 export const getAllGames = async (callback) => {
   const data = games;
@@ -14,45 +16,52 @@ export const getGameOptions = async (callback) => {
 };
 
 export const getAllListings = async (callback) => {
-  const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/listings')
-  const data = await response.json();
-  const values = Object.values(data);
-  console.log(data)
+  // const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/listings')
+  // const data = await response.json();
+  // const values = Object.values(data);
+  
+  // Lines above commented out for data usage restriction. Line below replaces with static data. 
+  const values = Object.values(listings);
+
   return callback(values);
 }
 
 export const getAllUsers = async (callback) => {
-  const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/users')
-  const data = await response.json();
-  const values = Object.values(data);
-  //console.log(data)
+  // const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/users')
+  // const data = await response.json();
+  // const values = Object.values(data);
+
+  // Lines above commented out for data usage restriction. Line below replaces with static data. 
+  const values = Object.values(users);
+
   return callback(values);
 }
 
 export const postListing = async (body) => {
-  const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/listings',
-    {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-  )
-  const json = await response.json() 
-  console.log(json)
+  // const response = await fetch('https://playertoo-43706.firebaseapp.com/api/v1/listings',
+  //   {
+  //     method: 'POST',
+  //     body: JSON.stringify(body),
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   }
+  // )
+  // const json = await response.json() 
+
+  console.log("Listing will post when REST routes wired back up.")
 };
 
 export const deleteListing = async (id) => {
-  const response = await fetch(`https://playertoo-43706.firebaseapp.com/api/v1/listings/${id}`,
-    {
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-  );
-  const json = await response.json()
-  console.log(json)
-  console.log('deleted')
+  // const response = await fetch(`https://playertoo-43706.firebaseapp.com/api/v1/listings/${id}`,
+  //   {
+  //     method: 'DELETE',
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   }
+  // );
+  // const json = await response.json()
+
+  console.log("Listing will delete when REST routes wired back up.")
 }
